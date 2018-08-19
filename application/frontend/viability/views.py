ac8c20734a09560e983c2168e1b7d96b925852ca
Colm Britton <colmjude@gmail.com>
2018-08-19 23:13:59 +0100

    Added a local authority choice page to the viability journey

--- application/frontend/viability/views.py
+++ application/frontend/viability/views.py
@@ -15,5 +15,13 @@ viability = Blueprint('viability', __name__, template_folder='templates', url_pr
 
 
 @viability.route('/')
-def index():
+def start():
   return render_template('v-start-page.html')
+
+@viability.route('/local-authority', methods=['GET', 'POST'])
+def local_authority():
+  datafile = "application/data/localauthorities.json"
+  if os.path.isfile( datafile ):
+    with open( datafile ) as data_file:
+      localauthorities = json.load(data_file) 
+  return render_template('v-local-authority.html', localauthorities=localauthorities['authorities'])
