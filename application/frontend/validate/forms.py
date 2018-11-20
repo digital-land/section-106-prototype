3ae92bbdd90eb11aca026abe861c3d8bb88107c6
Adam Shimali <adam.shimali@gmail.com>
2018-11-20 15:22:39 +0000

    Added select for csv file type to be validated and schema from development agreement contribution.

--- application/frontend/validate/forms.py
+++ application/frontend/validate/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import SelectField
 
 
 class UploadForm(FlaskForm):
@@ -8,3 +9,8 @@ class UploadForm(FlaskForm):
         FileRequired(),
         FileAllowed(['csv'], 'Only csv or files allowed')
     ])
+
+    validation_type = SelectField(u'Type of section 106 data file you want to validate',
+                                  choices=[('developer-agreement', 'Developer agreement'),
+                                           ('developer-agreement-contribution', 'Developer agreement contribution'),
+                                           ('developer-agreement-transation', 'Developer agreement transaction')])
--- application/frontend/validate/views.py
+++ application/frontend/validate/views.py
@@ -22,18 +22,9 @@ def validate():
             developer_agreement_csv = os.path.join(temp_dir, filename)
             file.save(developer_agreement_csv)
             data_directory = os.path.join(current_app.config['PROJECT_ROOT'], 'application', 'data')
-            developer_agreement_schema = os.path.join(data_directory, 'developer-agreement-schema.json')
-
-            # Colm at the moment this just validates against schema for first type of file (developer-agreement.csv),
-            # but it could be that the form that points here requires user to choose type
-            # e.g. developer-agreement.csv, developer-agreement-contribution.csv or developer-agreement-transaction.csv
-            # or there could be one page per type? Up to you.
-            # Also the report page dumps the report straight into page, sorry :)
-            # Try uploading tests/data/test-developer-agreement-invalid.csv and
-            # tests/data/test-developer-agreement-valid.csv to see the report.
-
-            report = goodtables.validate(developer_agreement_csv, schema=developer_agreement_schema)
-
+            schema_name = '%s-schema.json' % form.validation_type.data
+            schema = os.path.join(data_directory, schema_name)
+            report = goodtables.validate(developer_agreement_csv, schema=schema)
             return render_template('validation-report.html', filename=filename, report=report)
 
     return render_template('validate.html', form=form)
