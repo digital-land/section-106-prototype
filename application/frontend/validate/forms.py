53fe44ee121a314a94645ff6f3b392abfcb6574c
Adam Shimali <adam.shimali@gmail.com>
2018-11-22 10:00:53 +0000

    Split validation over 2 screens.

--- application/frontend/validate/forms.py
+++ application/frontend/validate/forms.py
@@ -3,14 +3,16 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import SelectField
 
 
+class ValidationSelectionForm(FlaskForm):
+
+    validation_type = SelectField(u'Type of section 106 data file you want to validate',
+                                  choices=[('developer-agreement', 'Developer agreement'),
+                                           ('developer-agreement-contribution', 'Developer agreement contribution'),
+                                           ('developer-agreement-transaction', 'Developer agreement transaction')])
+
 class UploadForm(FlaskForm):
 
     upload = FileField('file', validators=[
         FileRequired(),
         FileAllowed(['csv'], 'Only csv or files allowed')
     ])
-
-    validation_type = SelectField(u'Type of section 106 data file you want to validate',
-                                  choices=[('developer-agreement', 'Developer agreement'),
-                                           ('developer-agreement-contribution', 'Developer agreement contribution'),
-                                           ('developer-agreement-transaction', 'Developer agreement transaction')])
--- application/frontend/validate/views.py
+++ application/frontend/validate/views.py
@@ -2,16 +2,27 @@ import os
 import tempfile
 import goodtables
 
-from flask import Blueprint, render_template, current_app
-from werkzeug.utils import secure_filename
+from flask import Blueprint, render_template, current_app, url_for
+from werkzeug.utils import secure_filename, redirect
 
-from application.frontend.validate.forms import UploadForm
+from application.frontend.validate.forms import UploadForm, ValidationSelectionForm
 
 validators = Blueprint('validators', __name__, template_folder='templates')
 
 
 @validators.route('/validate', methods=['GET', 'POST'])
-def validate():
+def validation_selection():
+
+    form = ValidationSelectionForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for('validators.validate_by_type', validation_type=form.validation_type.data))
+
+    return render_template('validation-selection.html', form=form)
+
+
+@validators.route('/validate/<validation_type>', methods=['GET', 'POST'])
+def validate_by_type(validation_type):
 
     form = UploadForm()
 
@@ -22,12 +33,12 @@ def validate():
             developer_agreement_csv = os.path.join(temp_dir, filename)
             file.save(developer_agreement_csv)
             schema_directory = os.path.join(current_app.config['PROJECT_ROOT'], 'application', 'schema')
-            schema_name = '%s-schema.json' % form.validation_type.data
+            schema_name = '%s-schema.json' % validation_type
             schema = os.path.join(schema_directory, schema_name)
             report = goodtables.validate(developer_agreement_csv, schema=schema)
             return render_template('validation-report.html', filename=filename, report=report)
 
-    return render_template('validate.html', form=form)
+    return render_template('validate.html', form=form, validation_type=validation_type)
 
 
 
