9fd538a1237ba887aa40d422cc7d5aaf9f4c06aa
Colm Britton <colmjude@gmail.com>
2018-11-23 08:49:26 +0800

    More dummy data for cat breakdown

--- application/utils.py
+++ application/utils.py
@@ -1,11 +1,11 @@
 section_106_contribution_categories = [
-    {"id": "section-106:affordable-housing", "name": "Affordable housing", "amount": 5297451},
-    {"id": "section-106:education", "name": "Education", "amount": 3531634},
-    {"id": "section-106:health", "name": "Health", "amount": 2774855},
-    {"id": "section-106:highways",	"name": "Highways", "amount": 1765817},
-    {"id": "section-106:transport", "name": "Transport", "amount": 3783893},
-    {"id": "section-106:open-space", "name": "Open space", "amount": 2270336},
-    {"id": "section-106:community-facilities",	"name": "Community facilities", "amount": 1765817},
-    {"id": "section-106:green-infrastructure",	"name": "Green infrastructure", "amount": 2018076},
-    {"id": "section-106:other",	"name": "Other", "amount": 2018076}
+    {"id": "section-106:affordable-housing", "name": "Affordable housing", "amount": 8697451, "received": 5443911, "spent": 3298471, "outstanding": 3253540},
+    {"id": "section-106:education", "name": "Education", "amount": 5631634, "received": 4698153, "spent": 2334919, "outstanding": 933481},
+    {"id": "section-106:health", "name": "Health", "amount": 5474855, "received": 2894517, "spent": 1997000, "outstanding": 2580338},
+    {"id": "section-106:highways",	"name": "Highways", "amount": 4565817, "received": 4298731, "spent": 2018637, "outstanding": 267086},
+    {"id": "section-106:transport", "name": "Transport", "amount": 7383893, "received": 3987512, "spent": 4023291, "outstanding": 3396381},
+    {"id": "section-106:open-space", "name": "Open space", "amount": 3470336, "received": 2874190, "spent": 1576733, "outstanding": 596146},
+    {"id": "section-106:community-facilities",	"name": "Community facilities", "amount": 3765817, "received": 3001983, "spent": 1578290, "outstanding": 763834},
+    {"id": "section-106:green-infrastructure",	"name": "Green infrastructure", "amount": 4018076, "received": 2118076, "spent": 1782739, "outstanding": 1900000},
+    {"id": "section-106:other",	"name": "Other", "amount": 4018076, "received": 2781000, "spent": 1284933, "outstanding": 1237076}
 ]
