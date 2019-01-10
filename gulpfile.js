39b6522a8ad24ae0d2cd23bce4468a7fda32a788
Colm Britton <colmjude@gmail.com>
2019-01-10 14:02:14 +0800

    Upgraded gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -12,31 +12,51 @@ const config = {
   imgDestPath: "application/static/govuk_template/images"
 }
 
-// Delete our old stylesheets files
-gulp.task('clean-css', function () {
-  return gulp.src('application/static/stylesheets/**/*', {read: false})
+// Tasks used to generate latest stylesheets
+// =========================================
+const cleanCSS = () => 
+  gulp
+    .src('application/static/stylesheets/**/*', {read: false})
     .pipe(clean());
-});
+cleanCSS.description = `Delete old stylesheets files`;
 
 // compile scss to CSS
-gulp.task("scss", ['copy-css'], function() {
-	return gulp.src( config.scssPath + '/*.scss')
-	.pipe(sass({outputStyle: 'expanded',
-		includePaths: [ 'src/scss', 'src/govuk-frontend' ]})).on('error', sass.logError)
-	.pipe(gulp.dest(config.destPath))
-})
+const compileStylesheets = () =>
+  gulp
+    .src( config.scssPath + '/*.scss')
+	  .pipe(sass({outputStyle: 'expanded',
+		  includePaths: [ 'src/scss', 'src/govuk-frontend']}))
+      .on('error', sass.logError)
+    .pipe(gulp.dest(config.destPath));
 
-// Watch src folder for changes
-gulp.task("watch", ["scss"], function () {
-  gulp.watch("src/scss/**/*", ["scss"])
-});
-
-gulp.task('copy-css', ['clean-css'], function() {
-  gulp.src('src/govuk-frontend/assets/**/*')
-    .pipe(gulp.dest(config.assetPath));
-  gulp.src('src/govuk/stylesheets/*.css')
+// Tasks for copying assets to application
+// ======================================
+const copyGovukStylesheets = () =>
+  gulp
+    .src('src/govuk/stylesheets/*')
     .pipe(gulp.dest(config.destPath + "/govuk"));
-});
+
+const copyGovukAssets = () => 
+  gulp
+    .src('src/govuk-frontend/assets/**/*')
+    .pipe(gulp.dest(config.assetPath));
+
+// Tasks to expose to CLI
+// ======================
+const latestStylesheets = gulp.series(
+  cleanCSS,
+  compileStylesheets,
+  gulp.parallel(
+    copyGovukStylesheets,
+    copyGovukAssets
+  )
+);
+latestStylesheets.description = `Generate the latest stylesheets`;
+
+// Watch for scss changes
+const watch = () => gulp.watch("src/scss/**/*", latestStylesheets);
+watch.description = `Watch all project .scss for changes, then rebuild stylesheets.`;
 
 // Set watch as default task
-gulp.task("default", ["watch"]);
+exports.default = watch;
+exports.stylesheets = latestStylesheets;
