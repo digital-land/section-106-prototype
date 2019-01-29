ceddeeec0d72a2d92a35610cddd73b5eab6b9244
Adam Shimali <adam.shimali@gmail.com>
2019-01-29 16:32:53 +0000

    Make sure file is one of acceptable developer contribution files.

--- application/frontend/validate/forms.py
+++ application/frontend/validate/forms.py
@@ -3,6 +3,10 @@ from wtforms import MultipleFileField
 from wtforms.validators import DataRequired, ValidationError
 
 
+allowed_files = set(['developer-agreement.csv',
+                     'developer-agreement-contribution.csv',
+                     'developer-agreement-transaction.csv'])
+
 class UploadForm(FlaskForm):
 
     upload = MultipleFileField('Files', validators=[DataRequired()])
@@ -12,4 +16,6 @@ class UploadForm(FlaskForm):
         files = field.data[data.name]
         if len(files) > 3:
             raise ValidationError('Upload a maximum of 3 files')
-        
+        files = set(files)
+        if not files <= allowed_files:
+            raise ValidationError(f"Only the following files can be uploaded {','.join(allowed_files)}")
